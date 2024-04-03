ea1cf0324a8f1499ffe8473d2b72d12ffb7a8f65
Taras-Osipenko <117122130+Taras-Osipenko@users.noreply.github.com>
2024-04-03 07:55:21 +0300

    5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,30 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+
+
+
+const button = document.querySelector(".change-color");
+
+const span = document.querySelector(".color");
+
+const body = document.querySelector("body");
+
+button.addEventListener("click", handleClick);
+
+function handleClick (event) {
+  console.log("Ha кнопку було натиснуто")
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
+  let randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+  
+  span.textContent = randomColor;
+
+};
+
+
+
+
