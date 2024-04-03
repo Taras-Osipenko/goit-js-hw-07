a8fae783becd557487c30e14a2e614d93e7216f8
Taras-Osipenko <117122130+Taras-Osipenko@users.noreply.github.com>
2024-04-03 17:31:49 +0300

    5

--- js/task-6.js
+++ js/task-6.js
@@ -1,10 +1,10 @@
-const controls = document.querySelector("controls");
+const controls = document.querySelector("#controls");
 const numberInput = document.querySelector("input");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxesEl = document.querySelector("#boxes");
 boxesEl.style.display = "flex";
-boxesEl.style.flexwrap = "wrap";
+boxesEl.style.flexWrap = "wrap";
 boxesEl.style.alignItems = "center";
 boxesEl.style.marginTop = '30px';
 
@@ -21,13 +21,13 @@ function toogleValue(event) {
 
 function createBoxes(amount) {
   boxesEl.innerHTML = "";
-  const newDiv = '';
-  let boxsize = 30;
+  let newDiv = '';
+  let boxSize = 30;
   for (let i = 1; i <= amount; i += 1) {
     
-    newDiv = `<div style="width: ${boxsize}px; height: ${boxsize}px; background: ${getRandomHexColor()}"></div>`;
+    newDiv = `<div style="width: ${boxSize}px; height: ${boxSize}px; background: ${getRandomHexColor()}"></div>`;
     boxesEl.insertAdjacentHTML("beforeend", newDiv);
-    boxsize += 10;
+    boxSize += 10;
   }
   return boxesEl;
 }
